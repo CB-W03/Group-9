ecd13a332640bc8aeae5f91f7d4fd645179e66d2
mkell01 <mkelly011001@gmail.com>
2024-07-11 10:36:47 +0100

    Added more attributes and did the getters and setters for each attribute.

--- demo/src/main/java/com/example/demo/Customer.java
+++ demo/src/main/java/com/example/demo/Customer.java
@@ -7,44 +7,64 @@ import jakarta.persistence.Id;
 public class Customer {
 
     @Id
-    private int custid;
+    private int custID;
 
-    private String name;
+    private String custName;
 
     private boolean accountBlocked;
 
-    public Customer() {
+    private String acctType;
 
-    }
+    private String sortCode;
 
-    public Customer(int custid, String name,boolean accountBlocked) {
-        this.custid = custid;
-        this.name = name;
+    private String acctNum;
+
+    private String iban;
+
+    private String bic;
+
+    private double balance;
+
+    public Customer(int custID, String custName, boolean accountBlocked, String acctType, String sortCode,
+                    String acctNum, String iban, String bic, double balance) {
+        this.custID = custID;
+        this.custName = custName;
         this.accountBlocked=accountBlocked;
+        this.acctType=acctType;
+        this.sortCode=sortCode;
+        this.acctNum=acctNum;
+        this.iban=iban;
+        this.bic=bic;
+        this.balance=balance;
     }
 
-    public int getCustId() {
-        return custid;
+    public String getAcctType() {return acctType;}
+    public String getSortCode() {return sortCode;}
+    public String getAcctNum() {return acctNum;}
+    public String getIban() {return iban;}
+    public String getBic() {return bic;}
+    public int getCustID() {
+        return custID;
     }
-
-    public String getName() {
-        return name;
+    public String getCustName() {
+        return custName;
     }
-
     public boolean getBlockedStatus() {
         return accountBlocked;
     }
+    public double getBalance(){return balance;}
 
+    public void setAcctType(String acctType) {this.acctType=acctType;}
+    public void setSortCode(String sortCode) {this.sortCode=sortCode;}
+    public void setAcctNum(String acctNum){this.acctNum=acctNum;}
+    public void setIban(String iban){this.iban=iban;}
+    public void setBic(String bic){this.bic=bic;}
     public void setBlockedStatus(boolean accountBlocked){
         this.accountBlocked=accountBlocked;
     }
-
-    public void setCustId(int custid) {
-        this.custid = custid;
+    public void setCustID(int custID) {this.custID = custID; }
+    public void setCustName(String custName) {this.custName = custName;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setBalance(double balance){this.balance=balance;}
 
 }
